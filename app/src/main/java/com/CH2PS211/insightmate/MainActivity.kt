3fdb7e9f6e638481f6ade659bad4dc5f90ccb209
Abdul Aziz <doelziez212@gmail.com>
2023-12-19 19:11:38 +0700

    fix money detector, add app logo and navigation bottom icon

--- app/src/main/java/com/CH2PS211/insightmate/MainActivity.kt
+++ app/src/main/java/com/CH2PS211/insightmate/MainActivity.kt
@@ -1,40 +1,24 @@
 package com.CH2PS211.insightmate
 
 import android.Manifest
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.Toast
-import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.viewModels
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.core.net.toUri
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.CH2PS211.insightmate.CameraActivity.Companion.CAMERAX_RESULT
-import com.CH2PS211.insightmate.data.ResultState
 import com.CH2PS211.insightmate.databinding.ActivityMainBinding
-import com.CH2PS211.insightmate.util.getImageUri
-import com.CH2PS211.insightmate.util.uriToFile
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var navView: BottomNavigationView
-
-    private var currentImageUri: Uri? = null
-    private val viewModel by viewModels<MainViewModel> {
-        ViewModelFactory.getInstance()
-    }
 
     private val requestPermissionLauncher =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
--- app/src/main/java/com/CH2PS211/insightmate/MainViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.CH2PS211.insightmate
-
-import androidx.lifecycle.ViewModel
-import com.CH2PS211.insightmate.data.UploadRepository
-import java.io.File
-
-class MainViewModel(private val repository: UploadRepository) : ViewModel() {
-
-}
